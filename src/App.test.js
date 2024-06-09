e96ef23ae5c8a8046eef4a915c17d5ba6a600af9
ungpyolee <uplee114@gmail.com>
2024-06-09 23:44:28 +0900

    App.test.js update

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom'; // MemoryRouter를 추가
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
-import { act } from 'react-dom/test-utils'; // 변경 전
+import { act } from 'react'; // react에서 act를 가져옵니다.
 
 test('renders learn react link', () => {
     act(() => {
