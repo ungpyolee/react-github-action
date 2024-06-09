bd7be344f0957fc9e68f5be588f8487f88d7cbb0
ungpyolee <uplee114@gmail.com>
2024-06-09 23:42:09 +0900

    MemoryRouter 추가

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; // MemoryRouter를 추가
 import App from './App';
 import { act } from 'react-dom/test-utils'; // 변경 전
 
 test('renders learn react link', () => {
     act(() => {
-        render(<App />);
+        render(
+            <MemoryRouter>
+                <App />
+            </MemoryRouter>
+        );
     });
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
