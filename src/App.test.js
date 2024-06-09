3e30640e7f6ed8f64911bed8e44534eb41fde781
ungpyolee <uplee114@gmail.com>
2024-06-09 23:38:28 +0900

    app.test.js update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { act } from 'react-dom/test-utils'; // 변경 전
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    act(() => {
+        render(<App />);
+    });
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
