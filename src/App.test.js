93df0b3bea5b5543a9c62ad05e065a149d3a40cc
ungpyolee <uplee114@gmail.com>
2024-06-10 10:35:38 +0900

    app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
-import { act } from 'react'; // react에서 act를 가져옵니다.
 
 test('renders learn react link', () => {
-    act(() => {
-        render(
-            <MemoryRouter>
-                <App />
-            </MemoryRouter>
-        );
-    });
+    render(
+        <MemoryRouter>
+            <App />
+        </MemoryRouter>
+    );
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
 });
